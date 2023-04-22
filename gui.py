4ec87434defacd6ca36f19bcedfbc80fa6cb2e91
khandelwala812 <avikwal123@gmail.com>
2023-04-22 16:23:32 -0400

    Added C++ Dstar code and exe

--- gui.py
+++ gui.py
@@ -1,76 +1,184 @@
-from tkinter import *
+import pygame
 import numpy as np
+from dstar import findPath
 
-INITIAL_ROWS = 15
-INITIAL_COLS = 15
-grid = np.zeros((INITIAL_ROWS, INITIAL_COLS))
+# Initializing window using pygame
+WIDTH = 800
+WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("D* Visualization")
 
-window = Tk()
-window.title("DStar Visualization")
-window.geometry("{}x{}".format(800, 700))
-
-# create all the main containers
-# top_frame = Frame(window, bg="white", width=800, height=50, pady=3)
-center = Frame(window, bg="white", width=50, height=40, padx=3, pady=3)
-details_frame = Frame(window, bg="white", width=50, height=40)
-
-center.place(relx=0.5, rely=0.5, anchor=CENTER)
-details_frame.grid(row=1, column=1, sticky="nsew")
-
-
-# function wrapper to modify grid's dimensions
-def modifyGrid(is_col, is_increasing):
-    def resize():
-        global grid
-
-        old_rows = len(grid)
-        old_cols = len(grid[0])
-        new_rows = old_rows + (1 if is_increasing else -1) if not is_col else 0
-        new_cols = old_cols + (1 if is_increasing else -1) if is_col else 0
-
-        new_grid = np.zeros((new_rows, new_cols))
-
-        for existing_x in range(min(old_rows, new_rows)):
-            for existing_y in range(min(old_cols, new_cols)):
-                new_grid[existing_x][existing_y] = grid[existing_x][existing_y]
-
-        grid = new_grid
-        # refresh()
-
-    return resize
+# Initializing colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
 
-# initialize buttons
-add_columns_btn = Button(details_frame, text="+", command=modifyGrid(True, True))
-remove_columns_btn = Button(details_frame, text="-", command=modifyGrid(True, False))
-add_rows_btn = Button(details_frame, text="+", command=modifyGrid(False, True))
-remove_rows_btn = Button(details_frame, text="-", command=modifyGrid(False, False))
+# Creating node class that represents each grid square
+class Node:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.width = width
+        self.total_rows = total_rows
+        self.value = 0
 
-# pack buttons
-add_columns_btn.pack()
-remove_columns_btn.pack()
-add_rows_btn.pack()
-remove_rows_btn.pack()
+    def get_position(self):
+        return self.row, self.col
 
-colors = ["black", "white", "pink"]
-for x in range(len(grid)):
-    center.columnconfigure(x)
-    center.rowconfigure(x)
-    for y in range(len(grid[x])):
-        boxWidth = 100
-        boxHeight = 100
-        box = Frame(center, bg=colors[0], width=boxWidth, height=boxHeight, borderwidth=2, relief="solid")
-        box.grid(row=x, column=y, padx=4, pady=4)
+    def is_open(self):
+        return self.color == WHITE
 
-        def toggleColors(toggled_box):
-            def onClick(e):
-                box_color = toggled_box.cget("bg")
-                new_index = colors.index(box_color) + 1
-                toggled_box.configure(bg=colors[new_index % len(colors)])
-                grid[x][y] = new_index % len(colors)
+    def is_barrier(self):
+        return self.color == BLACK
 
-            return onClick
+    def is_start(self):
+        return self.color == TURQUOISE
 
-        box.bind("<Button-1>", toggleColors(box))
+    def is_end(self):
+        return self.color == PURPLE
 
-window.mainloop()
+    def reset(self):
+        self.make_open()
+
+    def make_start(self):
+        self.color = TURQUOISE
+
+    def make_open(self):
+        self.value = 0
+        self.color = WHITE
+
+    def make_barrier(self):
+        self.value = 1
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = PURPLE
+
+    def draw_node(self, window):
+        pygame.draw.rect(window, self.color, pygame.Rect(self.x, self.y, self.width, self.width))
+
+
+# Making a 2D grid filled with node objects
+def make_grid(rows, width):
+    grid = np.empty((rows, rows), dtype=Node)
+    space = width // rows
+
+    for i in range(rows):
+        for j in range(rows):
+            node = Node(i, j, space, rows)
+            grid[i][j] = node
+
+    return grid
+
+
+# Drawing the lines on the screen that create the appearence of a grid
+def draw_grid(window, rows, width):
+    space = width // rows
+
+    for i in range(rows):
+        pygame.draw.line(window, GREY, [0, i * space], [width, i * space])
+        for j in range(rows):
+            pygame.draw.line(window, GREY, [j * space, 0], [j * space, width])
+
+
+# Drawing the grid and all of the nodes onto the screen with their corresponding color
+def draw(window, grid, rows, width):
+    window.fill(WHITE)
+
+    for row in grid:
+        for node in row:
+            node.draw_node(window)
+
+    draw_grid(window, rows, width)
+    pygame.display.update()
+
+
+# Getting the position in the grid where the mouse clicked
+def get_clicked_pos(pos, rows, width):
+    space = width // rows
+    y, x = pos
+
+    row = y // space
+    col = x // space
+
+    return row, col
+
+
+def main(window, width):
+    ROWS = 20  # Sets how many rows are in the grid/how large the grid is
+    grid = make_grid(ROWS, width)
+
+    # Both are normally None
+    start = grid[0][0]
+    end = grid[ROWS - 1][ROWS - 1]
+
+    # Initializing start and end nodes
+    start.make_start()
+    end.make_end()
+
+    running = True
+    while running:
+        draw(window, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+
+            # Checking if the left mouse button has been pressed
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+
+                if not 0 <= row < ROWS or not 0 <= col < ROWS:
+                    continue
+
+                node = grid[row][col]
+                if node.is_start() or node.is_end():
+                    continue
+
+                # Normally elif
+                if node.is_open():
+                    node.make_barrier()
+                else:
+                    node.make_open()
+
+            # Checking if the right mouse button has been pressed
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()  # Making the node white
+
+                """
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+                """
+
+            # Checking if the r button has been pressed
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_g:
+                    path = findPath(grid)
+                    print(path)
+                elif event.key == pygame.K_r:
+                    for row in grid:
+                        for node in row:
+                            node.reset()
+                            start.make_start()
+                            end.make_end()
+
+    pygame.quit()
+
+
+main(WINDOW, WIDTH)
